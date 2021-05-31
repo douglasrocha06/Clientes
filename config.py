7c6abdbb2f949140cfd0a63de23620351a100daf
Douglas Cristhian <63408725+douglasrocha06@users.noreply.github.com>
2021-05-31 15:30:54 -0300

    Update config.py

--- config.py
+++ config.py
@@ -10,16 +10,3 @@ app.config['MYSQL_DATABASE_DB'] = 'api_clientes'
 app.config['MYSQL_DATABASE_HOST'] = 'djlbanco.cxycaymkd24m.us-east-1.rds.amazonaws.com'
 mysql.init_app(app)
 
-mysql2 = MySQL()
-app2.config['MYSQL_DATABASE_USER'] = 'admin'
-app2.config['MYSQL_DATABASE_PASSWORD'] = '12345678'
-app2.config['MYSQL_DATABASE_DB'] = 'api_produtos'
-app2.config['MYSQL_DATABASE_HOST'] = 'djlbanco.cxycaymkd24m.us-east-1.rds.amazonaws.com'
-mysql2.init_app(app2)
-
-mysql3 = MySQL()
-app3.config['MYSQL_DATABASE_USER'] = 'admin'
-app3.config['MYSQL_DATABASE_PASSWORD'] = '12345678'
-app3.config['MYSQL_DATABASE_DB'] = 'api_inventario'
-app3.config['MYSQL_DATABASE_HOST'] = 'djlbanco.cxycaymkd24m.us-east-1.rds.amazonaws.com'
-mysql3.init_app(app3)
